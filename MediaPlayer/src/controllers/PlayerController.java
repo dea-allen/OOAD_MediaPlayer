93432fd2bb7e616ba82b83fa38c286ba10beb142
v-pravin <reach.pvv@gmail.com>
2016-04-19 16:16:04 -0600

    added code to play a sample file; not working though

--- MediaPlayer/src/controllers/PlayerController.java
+++ MediaPlayer/src/controllers/PlayerController.java
@@ -1,13 +1,39 @@
 package controllers;
 
-public class PlayerController 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import models.GuiModel;
+
+import view.GuiView;
+
+public class PlayerController
 {
+    private Media mediaObj;
+    private MediaPlayer mediaPlayer;
+    private GuiModel gui;
+    
+    public void PlayerController()
+    {
+        gui = GuiView.getView(null).getGuiModel();
+    }
+
     public void play()
     {
-        String test = "test";
+        try {
+            // translate local relative path to an URI
+            mediaObj = new Media(new File("./mediaSamples/allegro.mp3").toURI().toURL().toExternalForm());
+        }
+        catch (Exception ex) {
+           Logger.getLogger(ModuleController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        mediaPlayer = new MediaPlayer(mediaObj);
+        mediaPlayer.play();
     }
     public void stop()
     {
-        
+        mediaPlayer.stop();
     }
 }
